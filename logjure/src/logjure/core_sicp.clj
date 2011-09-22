a77412340fc761d638a6d803dc63b8ee0ebf98a3
Piotr Zielinski <zielinski_piotr@hotmail.com>
2011-09-21 21:39:13 -0400

    compound queries;

--- logjure/src/logjure/core_sicp.clj
+++ logjure/src/logjure/core_sicp.clj
@@ -395,6 +395,24 @@
 (defn extend-frame [variable value frame]
   (cons-pair (make-binding variable value) frame))
 
+(defn instantiate
+  "To instantiate an expression, we copy it, replacing any variables in the expression by their values in a given frame.
+The values are themselves instantiated, since they could contain variables (for example, if ?x in exp is bound to ?y
+as the result of unification and ?y is in turn bound to 5). The action to take if a variable cannot be instantiated is
+given by a procedural argument to instantiate."
+  [exp frame unbound-var-handler]
+  (let [copy 
+        (fn copy [exp]
+          (cond (variable? exp)
+                (let [the-binding (binding-in-frame exp frame)]
+                  (if the-binding
+                    (copy (binding-value the-binding))
+                    (unbound-var-handler exp frame)))
+                (pair? exp)
+                  (cons-pair (copy (car exp)) (copy (cdr exp)))
+                :else exp))]
+        (copy exp))
+  )
 
 ;---------------------------------------------------------------------------------------------------
 ; ASSERTIONS
@@ -437,7 +455,16 @@
 
 (declare extend-if-consistent qeval)
 
-(defn pattern-match [pat dat frame]
+(defn pattern-match
+  "The basic pattern matcher returns either the symbol failed or an extension of the given frame. The basic idea of
+the matcher is to check the pattern against the data, element by element, accumulating bindings for the pattern
+variables. If the pattern and the data object are the same, the match succeeds and we return the frame of bindings
+accumulated so far. Otherwise, if the pattern is a variable we extend the current frame by binding the variable to the
+data, so long as this is consistent with the bindings already in the frame. If the pattern and the data are both pairs,
+we (recursively) match the car of the pattern against the car of the data to produce a frame; in this frame we then
+match the cdr of the pattern against the cdr of the data. If none of these cases are applicable, the match fails and we
+return the symbol failed."
+  [pat dat frame]
   (cond (eq? frame 'failed) 'failed
         (equal? pat dat) frame
         (variable? pat) (extend-if-consistent pat dat frame)
@@ -449,21 +476,47 @@
         :else 'failed)
   )
 
-(defn extend-if-consistent [var dat frame]
-  (let [binding (binding-in-frame var frame)]
+(defn extend-if-consistent
+  "Here is the procedure that extends a frame by adding a new binding, if this is consistent with the bindings already in
+the frame.
+If there is no binding for the variable in the frame, we simply add the binding of the variable to the data. Otherwise
+we match, in the frame, the data against the value of the variable in the frame. If the stored value contains only
+constants, as it must if it was stored during pattern matching by extend-if-consistent, then the match simply
+tests whether the stored and new values are the same. If so, it returns the unmodified frame; if not, it returns a
+failure indication. The stored value may, however, contain pattern variables if it was stored during unification (see
+section 4.4.4.4). The recursive match of the stored pattern against the new data will add or check bindings for the
+variables in this pattern. For example, suppose we have a frame in which ?x is bound to (f ?y) and ?y is unbound,
+and we wish to augment this frame by a binding of ?x to (f b). We look up ?x and find that it is bound to (f ?y).
+This leads us to match (f ?y) against the proposed new value (f b) in the same frame. Eventually this match
+extends the frame by adding a binding of ?y to b. ?X remains bound to (f ?y). We never modify a stored binding
+and we never store more than one binding for a given variable."
+  [variable dat frame]
+  (let [binding (binding-in-frame variable frame)]
     (if binding
       (pattern-match (binding-value binding) dat frame) ;TRAMPOLINE or move to pattern-match!!!!!!!!!!!!!!
-      (extend-frame var dat frame)))
+      (extend-frame variable dat frame)))
   )
 
-(defn check-an-assertion [assertion query-pat query-frame]
+(defn check-an-assertion
+  "Check-an-assertion takes as arguments a pattern, a data object (assertion), and a frame and returns either a oneelement
+stream containing the extended frame or the-empty-stream if the match fails."
+  [assertion query-pat query-frame]
   (let [match-result (pattern-match query-pat assertion query-frame)]
     (if (eq? match-result 'failed)
       the-empty-stream
       (singleton-stream match-result)))
   )
 
-(defn find-assertions [pattern frame]
+(defn find-assertions
+  "Find-assertions, called by simple-query (section 4.4.4.2), takes as input a pattern and a frame. It returns a
+stream of frames, each extending the given one by a data-base match of the given pattern. It uses fetchassertions
+(section 4.4.4.5) to get a stream of all the assertions in the data base that should be checked for a match
+against the pattern and the frame. The reason for fetch-assertions here is that we can often apply simple tests
+that will eliminate many of the entries in the data base from the pool of candidates for a successful match. The
+system would still work if we eliminated fetch-assertions and simply checked a stream of all assertions in the
+data base, but the computation would be less efficient because we would need to make many more calls to the
+matcher."
+  [pattern frame]
   (stream-flatmap 
     (fn [datum] (check-an-assertion datum pattern frame))
     (fetch-assertions pattern frame))
@@ -491,23 +544,60 @@
 
 (put 'and 'qeval conjoin)
 
+(defn disjoin [disjuncts frame-stream]
+  (if (empty-disjunction? disjuncts)
+    the-empty-stream
+    (interleave-delayed
+      (qeval (first-disjunct disjuncts) frame-stream)
+      (delay (disjoin (rest-disjuncts disjuncts) frame-stream))))
+  )
+
+(put 'or 'qeval disjoin)
+
+(defn negate [operands frame-stream]
+  (stream-flatmap
+    (fn [frame]
+      (if (stream-null? (qeval (negated-query operands) (singleton-stream frame)))
+        (singleton-stream frame)
+        the-empty-stream))
+    frame-stream)
+  )
+
+(put 'not 'qeval negate)
+
+(defn execute [exp]
+  (apply 
+    (eval 
+      (predicate exp)
+      );user-initial-environment) 
+    (args exp))
+  )
+
+(defn lisp-value [call frame-stream]
+  (stream-flatmap
+    (fn [frame]
+      (if (execute
+            (instantiate
+              call
+              frame
+              (fn [v f]
+                (error "Unknown pat var -- LISP-VALUE" v))))
+        (singleton-stream frame)
+        the-empty-stream))
+    frame-stream
+    )
+  )
+
+(put 'lisp-value 'qeval lisp-value)
+
+(defn always-true [ignore frame-stream] 
+  frame-stream
+  )
+
+(put 'always-true 'qeval always-true)
 
 ;---------------------------------------------------------------------------------------------------
 ; REST
-
-(defn instantiate [exp frame unbound-var-handler]
-  (let [copy 
-        (fn copy [exp]
-          (cond (variable? exp)
-                (let [the-binding (binding-in-frame exp frame)]
-                  (if the-binding
-                    (copy (binding-value the-binding))
-                    (unbound-var-handler exp frame)))
-                (pair? exp)
-                  (cons-pair (copy (car exp)) (copy (cdr exp)))
-                :else exp))]
-        (copy exp))
-  )
 
 (defn get-type [query]
   (car query)
