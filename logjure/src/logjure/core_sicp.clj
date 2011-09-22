f5db81a5fc0d88bd586beea461cc9e37d5845265
Piotr Zielinski <zielinski_piotr@hotmail.com>
2011-09-22 10:15:03 -0400

    assertion/rule storage;

--- logjure/src/logjure/core_sicp.clj
+++ logjure/src/logjure/core_sicp.clj
@@ -9,18 +9,6 @@
 (defn prompt-for-input [input-prompt]
   )
 
-(defn assertion-to-be-added? [exp]
-  )
-
-(defn add-assertion-body [exp]
-  )
-
-(defn add-rule-or-assertion! [x]
-  )
-
-(defn apply-rules [query-pattern frame]
-  )
-
 ;---------------------------------------------------------------------------------------------------
 ; CLOJURE INTERFACE
 
@@ -276,12 +264,32 @@
 ;---------------------------------------------------------------------------------------------------
 ; SYNTAX
 
+(defn exp-type [exp]
+  (if (pair? exp)
+    (car exp)
+    (error "Unknown expression TYPE" exp))
+  )
+
+(defn exp-contents [exp]
+  (if (pair? exp)
+    (cdr exp)
+    (error "Unknown expression CONTENTS" exp))
+  )
+
 (defn variable? [exp]
   (tagged-list? exp '?)
   )
 
 (defn constant-symbol? [exp] 
   (symbol? exp)
+  )
+
+(defn assertion-to-be-added? [exp]
+  (eq? (exp-type exp) 'assert!)
+  )
+
+(defn add-assertion-body [exp]
+  (car (exp-contents exp))
   )
 
 (defn empty-conjunction? [exps] 
@@ -431,6 +439,10 @@ given by a procedural argument to instantiate."
   THE-ASSERTIONS
   )
 
+(defn indexable? [pat]
+  (or (constant-symbol? (car pat)) (variable? (car pat)))
+  )
+
 (defn use-index? [pat]
   (constant-symbol? (car pat))
   )
@@ -452,7 +464,17 @@ given by a procedural argument to instantiate."
   (get-stream (index-key-of pattern) 'assertion-stream)
   )
 
-(defn fetch-assertions [pattern frame]
+(defn fetch-assertions
+  "One important problem in designing logic programming languages is that of arranging things so that as few
+irrelevant data-base entries as possible will be examined in checking a given pattern. In our system, in addition to
+storing all assertions in one big stream, we store all assertions whose cars are constant symbols in separate streams,
+in a table indexed by the symbol. To fetch an assertion that may match a pattern, we first check to see if the car of
+the pattern is a constant symbol. If so, we return (to be tested using the matcher) all the stored assertions that have
+the same car. If the pattern's car is not a constant symbol, we return all the stored assertions. Cleverer methods
+could also take advantage of information in the frame, or try also to optimize the case where the car of the pattern
+is not a constant symbol. We avoid building our criteria for indexing (using the car, handling only the case of
+constant symbols) into the program; instead we call on predicates and selectors that embody our criteria."
+  [pattern frame]
   (if (use-index? pattern)
     (get-indexed-assertions pattern)
     (get-all-assertions))
@@ -470,10 +492,58 @@ given by a procedural argument to instantiate."
     (get-stream '? 'rule-stream))
   )
 
-(defn fetch-rules [pattern frame]
+(defn fetch-rules
+  "Rules are stored similarly, using the car of the rule conclusion. Rule conclusions are arbitrary patterns, however, so
+they differ from assertions in that they can contain variables. A pattern whose car is a constant symbol can match
+rules whose conclusions start with a variable as well as rules whose conclusions have the same car. Thus, when
+fetching rules that might match a pattern whose car is a constant symbol we fetch all rules whose conclusions start
+with a variable as well as those whose conclusions have the same car as the pattern. For this purpose we store all
+rules whose conclusions start with a variable in a separate stream in our table, indexed by the symbol ?."
+  [pattern frame]
   (if (use-index? pattern)
     (get-indexed-rules pattern)
     (get-all-rules))
+  )
+
+(defn store-assertion-in-index [assertion]
+  (if (indexable? assertion)
+    (let [key (index-key-of assertion)]
+      (let [current-assertion-stream (get-stream key 'assertion-stream)]
+        (put 
+          key 
+          'assertion-stream
+          (cons-stream assertion current-assertion-stream)))))
+  )
+
+(defn store-rule-in-index [rule]
+  (let [pattern (conclusion rule)]
+    (if (indexable? pattern)
+      (let [key (index-key-of pattern)]
+        (let [current-rule-stream (get-stream key 'rule-stream)]
+          (put 
+            key
+            'rule-stream
+            (cons-stream rule current-rule-stream))))))
+  )
+
+(defn add-assertion! [assertion]
+  (store-assertion-in-index assertion)
+  (let [old-assertions THE-ASSERTIONS]
+    (set! THE-ASSERTIONS (cons-stream assertion old-assertions));ATOM!!!!!!!!!!!!
+    'ok)
+  )
+
+(defn add-rule! [rule]
+  (store-rule-in-index rule)
+  (let [old-rules THE-RULES]
+    (set! THE-RULES (cons-stream rule old-rules));ATOM!!!!!!!!!!!!
+    'ok)
+  )
+
+(defn add-rule-or-assertion! [assertion]
+  (if (rule? assertion)
+    (add-rule! assertion)
+    (add-assertion! assertion))
   )
 
 ;---------------------------------------------------------------------------------------------------
@@ -558,7 +628,7 @@ matcher."
   )
 
 ;---------------------------------------------------------------------------------------------------
-; COMPOUNT QUERY
+; COMPOUND QUERY
 
 ;AND
 (defn conjoin [conjuncts frame-stream]
@@ -647,6 +717,8 @@ recursive tree walk in which we substitute for the values of variables whenever 
          (tree-walk exp))
   )
 
+(declare unify-match)
+
 (defn extend-if-possible
   "In unification, as in one-sided pattern matching, we want to accept a proposed extension of the frame only if it is
 consistent with existing bindings. The procedure extend-if-possible used in unification is the same as the
@@ -669,11 +741,11 @@ by the equal? clause of unify-match."
   (let [binding (binding-in-frame var frame)]
     (cond 
       binding
-      (unify-match (binding-value binding) val frame)
+      (unify-match (binding-value binding) val frame);TRAMPOLINE/RECUR!!!!!!!!!!!!!!!
       (variable? val) ; ***
       (let [binding (binding-in-frame val frame)]
         (if binding
-          (unify-match var (binding-value binding) frame)
+          (unify-match var (binding-value binding) frame);TRAMPOLINE/RECUR!!!!!!!!!!!!!!!
           (extend-frame var val frame)))
       (depends-on? val var frame) ; ***
       'failed
@@ -699,7 +771,7 @@ variable."
     (and (pair? p1) (pair? p2))
     (unify-match (cdr p1)
                  (cdr p2)
-                 (unify-match (car p1)
+                 (unify-match (car p1);RECUR!!!!!!!!!!!!!!!
                               (car p2)
                               frame))
     :else 'failed
