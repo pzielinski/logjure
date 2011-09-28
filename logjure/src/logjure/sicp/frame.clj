cbec783ff7e836916b3171e013e98e28a69ba93e
Piotr Zielinski <zielinski_piotr@hotmail.com>
2011-09-28 14:36:38 -0400

    added frame.combine-frames;

--- logjure/src/logjure/sicp/frame.clj
+++ logjure/src/logjure/sicp/frame.clj
@@ -1,14 +1,21 @@
 (ns logjure.sicp.frame
   )
 
-(defn make-empty-frame []
+(defn make-empty-frame 
+  []
   {}
   )
 
-(defn get-value-in-frame [variable frame]
+(defn get-value-in-frame 
+  [variable frame]
   (get frame variable)
   )
 
-(defn extend-frame [variable value frame]
+(defn extend-frame 
+  [variable value frame]
   (assoc frame variable value))
 
+(defn combine-frames 
+  [frame1 frame2]
+  (merge frame1 frame2)
+  )
