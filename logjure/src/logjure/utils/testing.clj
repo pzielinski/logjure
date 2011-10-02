ffe01c970ce3f5e5dcbae025ad4eb7e873918688
Piotr Zielinski <zielinski_piotr@hotmail.com>
2011-10-02 12:55:12 -0400

    recorder returns vector of args;

--- logjure/src/logjure/utils/testing.clj
+++ logjure/src/logjure/utils/testing.clj
@@ -16,7 +16,14 @@
   )
 
 (defmacro recorder [original-fn replacement-fn & test-fn]
-  `(let [records# (atom #{})
+  "Calls evaluates test-fn in binding where original-fn is replaced by replacement-fn 
+and argument for each original-fn call is recoreded in a vector, 
+the result is a vector of test-fn result and vector of orignal-fn arguments.
+Example: 
+(recorder inc #(+ % 1) (nth (apply concat (map list (iterate inc 1))) 0))
+produces result:
+[1 [1 2 3]]"
+  `(let [records# (atom [])
         record-fn# (fn [record#] (swap! records# conj record#))
         f-recording# (fn [x#] (do (record-fn# x#)) (~replacement-fn x#))
         ]
