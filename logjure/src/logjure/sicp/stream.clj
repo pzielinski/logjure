6a291b83c75f742df02dc6693f198f01719e0097
Piotr Zielinski <zielinski_piotr@hotmail.com>
2011-10-04 12:40:17 -0400

    stream - all functions that use cons-stream macro do not need recur; added flatten-stream (non interleave); more tests;

--- logjure/src/logjure/sicp/stream.clj
+++ logjure/src/logjure/sicp/stream.clj
@@ -4,71 +4,89 @@
 
 (def the-empty-stream nil)
 
-(defn stream-null? [stream]
+(defn stream-null? 
+  [stream]
   (null? stream)
   )
 
-(defmacro cons-stream [x s]
+(defmacro cons-stream 
+  [x s]
     `(cons-pair ~x (delay ~s))
   )
 
-(defn stream-car [stream] 
+(defn stream-car 
+  [stream] 
   (car stream)
   )
 
-(defn stream-cdr [stream] 
+(defn stream-cdr 
+  [stream] 
   (force (cdr stream))
   )
 
-(defn singleton-stream [x]
+(defn singleton-stream 
+  [x]
   (cons-stream x the-empty-stream)
   )
 
-(defn stream-append [s1 s2]
+(defn stream-append 
+  [s1 s2]
   (if (stream-null? s1)
     s2
     (cons-stream
       (stream-car s1)
-      (stream-append (stream-cdr s1) s2)));NEED RECUR !!!!!!!!!!!!!
+      (stream-append (stream-cdr s1) s2)))
   )
 
-(defn stream-append-delayed [s1 delayed-s2]
+(defn stream-append-delayed 
+  [s1 delayed-s2]
   (if (stream-null? s1)
     (force delayed-s2)
     (cons-stream
       (stream-car s1)
-      (stream-append-delayed (stream-cdr s1) delayed-s2)));NEED RECUR !!!!!!!!!!!!!
+      (stream-append-delayed (stream-cdr s1) delayed-s2)))
   )
 
-(defn interleave-delayed [s1 delayed-s2]
+(defn interleave-delayed 
+  [s1 delayed-s2]
   (if (stream-null? s1)
     (force delayed-s2)
     (cons-stream
       (stream-car s1)
-      (interleave-delayed (force delayed-s2) (delay (stream-cdr s1)))));NEED RECUR !!!!!!!!!!!!!
+      (interleave-delayed (force delayed-s2) (delay (stream-cdr s1)))))
   )
 
-(defn flatten-stream [stream]
+(defn flatten-interleave-stream 
+  [stream]
   (if (stream-null? stream)
     the-empty-stream
     (interleave-delayed
       (stream-car stream)
-      (delay (flatten-stream (stream-cdr stream)))));NEED RECUR !!!!!!!!!!!!!
+      (delay (flatten-interleave-stream (stream-cdr stream)))))
   )
 
-(defn stream-map [proc stream]
+(defn flatten-stream
+  "if the stream is infinite, this function will get stuck on it, 
+and following streams will never have a chance to contribute to result stream"
+  [stream]
+  (if (stream-null? stream)
+    the-empty-stream
+    (stream-append-delayed
+      (stream-car stream)
+      (delay (flatten-stream (stream-cdr stream)))))
+  )
+
+(defn stream-map 
+  [proc stream]
   (if (stream-null? stream)
     the-empty-stream
     (cons-stream 
       (proc (stream-car stream)) 
-      (stream-map proc (stream-cdr stream))));NEED RECUR !!!!!!!!!!!!!
+      (stream-map proc (stream-cdr stream))))
   )
 
-(defn stream-flatmap [proc stream]
-  (flatten-stream (stream-map proc stream))
-  )
-
-(defn stream-nth [n stream]
+(defn stream-nth 
+  [n stream]
   (if (stream-null? stream)
     the-empty-stream
     (if (= n 0)
--- logjure/test/logjure/sicp/stream_test.clj
+++ logjure/test/logjure/sicp/stream_test.clj
@@ -24,129 +24,13 @@
   (is (= the-empty-stream (stream-cdr (singleton-stream :a))))
 )
 
-(deftest test-stream-append
-  (let [s (stream-append (cons-stream :a (singleton-stream :b)) (singleton-stream :x))] 
-    (is (= :a (stream-car s)))
-    (is (= :b (stream-car (stream-cdr s))))
-    (is (= :x (stream-car (stream-cdr (stream-cdr s)))))
-    (is (= the-empty-stream (stream-car (stream-cdr (stream-cdr (stream-cdr s))))))
-  )
-)
-
-(deftest test-stream-append-delayed
-  (let [s (stream-append-delayed (cons-stream :a (singleton-stream :b)) (singleton-stream :x))] 
-    (is (= :a (stream-car s)))
-    (is (= :b (stream-car (stream-cdr s))))
-    (is (= :x (stream-car (stream-cdr (stream-cdr s)))))
-  )
-)
-
-(deftest test-interleave-delayed
-  (let [s1 (cons-stream :a (cons-stream :b (cons-stream :c (singleton-stream :d))))
-        s2 (cons-stream :x (cons-stream :y (cons-stream :z (singleton-stream :zz))))
-        s (interleave-delayed s1 s2)
-        ] 
-    (is (= :a (stream-car s)))
-    (is (= :x (stream-car (stream-cdr s))))
-    (is (= :b (stream-car (stream-cdr (stream-cdr s)))))
-    (is (= :y (stream-car (stream-cdr (stream-cdr (stream-cdr s))))))
-    (is (= :c (stream-car (stream-cdr (stream-cdr (stream-cdr (stream-cdr s)))))))
-    (is (= :z (stream-car (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr s))))))))
-    (is (= :d (stream-car (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr s)))))))))
-    (is (= :zz (stream-car (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr s))))))))))
-    (is (= the-empty-stream (stream-car (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr s)))))))))))
-    )
-  )
-
-(deftest test-flatten-stream
-  (let [s1 (cons-stream :a (cons-stream :b (cons-stream :c (singleton-stream :d))))
-        s2 (cons-stream :x (cons-stream :y (cons-stream :z (singleton-stream :zz))))
-        sX (cons-stream s1 (singleton-stream s2));stream of streams
-        s (flatten-stream sX)
-        ] 
-    (is (= :a (stream-car s)))
-    (is (= :x (stream-car (stream-cdr s))))
-    (is (= :b (stream-car (stream-cdr (stream-cdr s)))))
-    (is (= :y (stream-car (stream-cdr (stream-cdr (stream-cdr s))))))
-    (is (= :c (stream-car (stream-cdr (stream-cdr (stream-cdr (stream-cdr s)))))))
-    (is (= :z (stream-car (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr s))))))))
-    (is (= :d (stream-car (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr s)))))))))
-    (is (= :zz (stream-car (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr s))))))))))
-    (is (= the-empty-stream (stream-car (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr s)))))))))))
-    )
-  )
-
-(deftest test-flatten-stream-3
-  (let [s1 (cons-stream :a (cons-stream :b (singleton-stream :c)))
-        s2 (cons-stream :x (cons-stream :y (singleton-stream :z)))
-        s3 (cons-stream 1 (cons-stream 2 (singleton-stream 3)))
-        sX (cons-stream s1 (cons-stream s2 (singleton-stream s3)));stream of streams
-        s (flatten-stream sX)
-        ] 
-    (is (= :a (stream-car s)))
-    (is (= :x (stream-car (stream-cdr s))))
-    (is (= :b (stream-car (stream-cdr (stream-cdr s)))))
-    (is (= 1 (stream-car (stream-cdr (stream-cdr (stream-cdr s))))))
-    (is (= :c (stream-car (stream-cdr (stream-cdr (stream-cdr (stream-cdr s)))))))
-    (is (= :y (stream-car (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr s))))))))
-    (is (= 2 (stream-car (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr s)))))))))
-    (is (= :z (stream-car (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr s))))))))))
-    (is (= 3 (stream-car (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr s)))))))))))
-    (is (= the-empty-stream (stream-car (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr s))))))))))))
-    )
-  )
-
-(deftest test-stream-map
-  (let [s (stream-map #(+ % 10) (cons-stream 1 (cons-stream 2 (singleton-stream 3))))] 
-    (is (= 11 (stream-car s)))
-    (is (= 12 (stream-car (stream-cdr s))))
-    (is (= 13 (stream-car (stream-cdr (stream-cdr s)))))
-  )
-)
-
-(deftest test-stream-flatmap
-  (let [s1 (cons-stream 1 (cons-stream 2 (cons-stream 3 (singleton-stream 4))))
-        s (stream-flatmap #(cons-stream % s1) s1)
-        ;1 1 2 3 4
-        ;2 1 2 3 4
-        ;3 1 2 3 4
-        ;4 1 2 3 4
-        ] 
-    (is (= 1 (stream-car s)))
-    (is (= 2 (stream-car (stream-cdr s))))
-    (is (= 1 (stream-car (stream-cdr (stream-cdr s)))))
-    (is (= 3 (stream-car (stream-cdr (stream-cdr (stream-cdr s))))))
-    (is (= 2 (stream-car (stream-cdr (stream-cdr (stream-cdr (stream-cdr s)))))))
-    (is (= 1 (stream-car (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr s))))))))
-    (is (= 3 (stream-car (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr s)))))))))
-    )
-  )
-
 (deftest test-stream-nth
   (let [s (cons-stream :a (cons-stream :b (singleton-stream :c)))] 
     (is (= :a (stream-nth 0 s)))
     (is (= :b (stream-nth 1 s)))
     (is (= :c (stream-nth 2 s)))
     (is (= the-empty-stream (stream-nth 3 s)))
-  )
-)
-
-(defn deeply-nested [n]
-  (loop [n n result '(:bottom)]
-    (if (= n 0)
-      result
-      (recur (dec n) (list result)))))
-
-(defn make-lazy-int-stream [n]
-  (cons-stream n (delay (make-lazy-int-stream (inc n))))
-  )
-
-(deftest test-stream-nth
-  (let [s (cons-stream :a (cons-stream :b (singleton-stream :c)))] 
-    (is (= :a (stream-nth 0 s)))
-    (is (= :b (stream-nth 1 s)))
-    (is (= :c (stream-nth 2 s)))
-    (is (= the-empty-stream (stream-nth 3 s)))
+    (is (= 10001 (stream-nth 10000 (seq-to-stream (iterate inc 1)))))
   )
 )
 
@@ -161,5 +45,218 @@
     (is (= 10001 (stream-nth 10000 (seq-to-stream (iterate inc 1)))))
   )
 )
+
+(deftest test-stream-append
+  (let [s (stream-append (cons-stream :a (singleton-stream :b)) (singleton-stream :x))] 
+    (is (= :a (stream-car s)))
+    (is (= :b (stream-car (stream-cdr s))))
+    (is (= :x (stream-car (stream-cdr (stream-cdr s)))))
+    (is (= the-empty-stream (stream-car (stream-cdr (stream-cdr (stream-cdr s))))))
+    (is (= :x (stream-nth 10000 (stream-append (seq-to-stream (take 10000 (iterate inc 1))) (singleton-stream :x)))))
+  )
+)
+
+(deftest test-stream-append-delayed
+  (let [s (stream-append-delayed (cons-stream :a (singleton-stream :b)) (singleton-stream :x))] 
+    (is (= :a (stream-car s)))
+    (is (= :b (stream-car (stream-cdr s))))
+    (is (= :x (stream-car (stream-cdr (stream-cdr s)))))
+    (is (= the-empty-stream (stream-car (stream-cdr (stream-cdr (stream-cdr s))))))
+    (is (= :x (stream-nth 10000 (stream-append-delayed (seq-to-stream (take 10000 (iterate inc 1))) (singleton-stream :x)))))
+  )
+)
+
+(deftest test-interleave-delayed
+  (let [s1 (cons-stream :a (cons-stream :b (cons-stream :c (singleton-stream :d))))
+        s2 (cons-stream :x (cons-stream :y (cons-stream :z (singleton-stream :zz))))
+        s (interleave-delayed s1 s2)] 
+    (is (= :a (stream-car s)))
+    (is (= :x (stream-car (stream-cdr s))))
+    (is (= :b (stream-car (stream-cdr (stream-cdr s)))))
+    (is (= :y (stream-car (stream-cdr (stream-cdr (stream-cdr s))))))
+    (is (= :c (stream-car (stream-cdr (stream-cdr (stream-cdr (stream-cdr s)))))))
+    (is (= :z (stream-car (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr s))))))))
+    (is (= :d (stream-car (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr s)))))))))
+    (is (= :zz (stream-car (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr s))))))))))
+    (is (= the-empty-stream (stream-car (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr s)))))))))))
+    )
+  (let [s1 (seq-to-stream (iterate inc 1))
+        s2 (seq-to-stream (repeat :a))
+        s (interleave-delayed s1 s2)]
+    (is (= 1 (stream-nth 0 s)))
+    (is (= :a (stream-nth 1 s)))
+    (is (= 2 (stream-nth 2 s)))
+    (is (= :a (stream-nth 3 s)))
+    (is (= 3 (stream-nth 4 s)))
+    (is (= 5001 (stream-nth 10000 s)))
+    (is (= :a (stream-nth 10001 s)))
+    )
+  )
+
+(deftest test-flatten-interleave-stream-2
+  (let [s1 (cons-stream :a (cons-stream :b (cons-stream :c (singleton-stream :d))))
+        s2 (cons-stream :x (cons-stream :y (cons-stream :z (singleton-stream :zz))))
+        sX (cons-stream s1 (singleton-stream s2));stream of streams
+        s (flatten-interleave-stream sX)
+        ] 
+    (is (= :a (stream-car s)))
+    (is (= :x (stream-car (stream-cdr s))))
+    (is (= :b (stream-car (stream-cdr (stream-cdr s)))))
+    (is (= :y (stream-car (stream-cdr (stream-cdr (stream-cdr s))))))
+    (is (= :c (stream-car (stream-cdr (stream-cdr (stream-cdr (stream-cdr s)))))))
+    (is (= :z (stream-car (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr s))))))))
+    (is (= :d (stream-car (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr s)))))))))
+    (is (= :zz (stream-car (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr s))))))))))
+    (is (= the-empty-stream (stream-car (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr s)))))))))))
+    )
+  )
+
+(deftest test-flatten-interleave-stream-3
+  (let [s1 (cons-stream :a (cons-stream :b (singleton-stream :c)))
+        s2 (cons-stream :x (cons-stream :y (singleton-stream :z)))
+        s3 (cons-stream 1 (cons-stream 2 (singleton-stream 3)))
+        sX (cons-stream s1 (cons-stream s2 (singleton-stream s3)));stream of streams
+        s (flatten-interleave-stream sX)
+        ] 
+    (is (= :a (stream-car s)))
+    (is (= :x (stream-car (stream-cdr s))))
+    (is (= :b (stream-car (stream-cdr (stream-cdr s)))))
+    (is (= 1 (stream-car (stream-cdr (stream-cdr (stream-cdr s))))))
+    (is (= :c (stream-car (stream-cdr (stream-cdr (stream-cdr (stream-cdr s)))))))
+    (is (= :y (stream-car (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr s))))))))
+    (is (= 2 (stream-car (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr s)))))))))
+    (is (= :z (stream-car (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr s))))))))))
+    (is (= 3 (stream-car (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr s)))))))))))
+    (is (= the-empty-stream (stream-car (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr s))))))))))))
+    )
+  )
+
+(deftest test-flatten-interleave-stream-1-infinite
+  (let [s1 (seq-to-stream (iterate inc 1))
+        sX (cons-stream s1 the-empty-stream);stream of streams
+        s (flatten-interleave-stream sX)]
+    (is (= 1 (stream-nth 0 s)))
+    (is (= 2 (stream-nth 1 s)))
+    (is (= 10001 (stream-nth 10000 s)))
+    )
+  )
+
+(deftest test-flatten-interleave-stream-2-infinite
+  (let [s1 (seq-to-stream (iterate inc 1))
+        s2 (seq-to-stream (repeat :a))
+        sX (cons-stream s1 (cons-stream s2 the-empty-stream));stream of streams
+        s (flatten-interleave-stream sX)]
+    (is (= 1 (stream-nth 0 s)))
+    (is (= :a (stream-nth 1 s)))
+    (is (= 2 (stream-nth 2 s)))
+    (is (= :a (stream-nth 3 s)))
+    (is (= 5001 (stream-nth 10000 s)))
+    (is (= :a (stream-nth 10001 s)))
+    )
+  )
+
+(deftest test-flatten-interleave-stream-3-infinite
+  (let [s1 (seq-to-stream (iterate inc 1))
+        s2 (seq-to-stream (repeat :a))
+        s3 (seq-to-stream (repeat :x))
+        sX (cons-stream s1 (cons-stream s2 (singleton-stream s3)));stream of streams
+        s (flatten-interleave-stream sX)]
+    (is (= 1 (stream-nth 0 s)))
+    (is (= :a (stream-nth 1 s)))
+    (is (= 2 (stream-nth 2 s)))
+    (is (= :x (stream-nth 3 s)))
+    (is (= 3 (stream-nth 4 s)))
+    (is (= :a (stream-nth 5 s)))
+    (is (= 4 (stream-nth 6 s)))
+    (is (= :x (stream-nth 7 s)))
+    (is (= 5 (stream-nth 8 s)))
+    (is (= 5001 (stream-nth 10000 s)))
+    (is (= :a (stream-nth 10001 s)))
+    (is (= 5002 (stream-nth 10002 s)))
+    (is (= :x (stream-nth 10003 s)))
+    )
+  )
+
+(deftest test-flatten-stream-1-infinite
+  (let [s1 (seq-to-stream (iterate inc 1))
+        sX (cons-stream s1 the-empty-stream);stream of streams
+        s (flatten-stream sX)]
+    (is (= 1 (stream-nth 0 s)))
+    (is (= 2 (stream-nth 1 s)))
+    (is (= 10001 (stream-nth 10000 s)))
+    )
+  )
+
+(deftest test-flatten-stream-2
+  (let [s1 (seq-to-stream (take 3 (iterate inc 1)))
+        s2 (seq-to-stream (repeat :a))
+        sX (cons-stream s1 (cons-stream s2 the-empty-stream));stream of streams
+        s (flatten-stream sX)]
+    (is (= 1 (stream-nth 0 s)))
+    (is (= 2 (stream-nth 1 s)))
+    (is (= 3 (stream-nth 2 s)))
+    (is (= :a (stream-nth 4 s)))
+    (is (= :a (stream-nth 10000 s)))
+    (is (= :a (stream-nth 10001 s)))
+    )
+  )
+
+(deftest test-flatten-stream-3
+  (let [s1 (seq-to-stream (take 3(iterate inc 1)))
+        s2 (seq-to-stream (take 3 (repeat :a)))
+        s3 (seq-to-stream (iterate inc 1))
+        sX (cons-stream s1 (cons-stream s2 (singleton-stream s3)));stream of streams
+        s (flatten-stream sX)]
+    (is (= 1 (stream-nth 0 s)))
+    (is (= 2 (stream-nth 1 s)))
+    (is (= 3 (stream-nth 2 s)))
+    (is (= :a (stream-nth 3 s)))
+    (is (= :a (stream-nth 4 s)))
+    (is (= :a (stream-nth 5 s)))
+    (is (= 1 (stream-nth 6 s)))
+    (is (= 10000 (stream-nth 10005 s)))
+    )
+  )
+
+(deftest test-stream-map
+  (let [s (stream-map #(+ % 10) (cons-stream 1 (cons-stream 2 (singleton-stream 3))))] 
+    (is (= 11 (stream-car s)))
+    (is (= 12 (stream-car (stream-cdr s))))
+    (is (= 13 (stream-car (stream-cdr (stream-cdr s)))))
+    (is (= the-empty-stream (stream-car (stream-cdr (stream-cdr (stream-cdr s))))))
+  )
+)
+
+(deftest test-stream-map-infinite
+  (let [s (stream-map #(+ % 10) (seq-to-stream (iterate inc 1)))] 
+    (is (= 11 (stream-nth 0 s)))
+    (is (= 10011 (stream-nth 10000 s)))
+  )
+)
+
+(deftest test-stream-map-proc-vector
+  (let [s (stream-map (fn [x] [x :a]) (seq-to-stream (iterate inc 1)))] 
+    (is (= [1 :a] (stream-nth 0 s)))
+    (is (= [10001 :a] (stream-nth 10000 s)))
+  )
+)
+
+(deftest test-stream-flatmap-interleave
+  (let [s1 (cons-stream 1 (cons-stream 2 (cons-stream 3 (singleton-stream 4))))
+        s (flatten-interleave-stream (stream-map #(cons-stream % s1) s1))
+        ;1 1 2 3 4
+        ;2 1 2 3 4
+        ;3 1 2 3 4
+        ;4 1 2 3 4
+        ] 
+    (is (= 1 (stream-car s)))
+    (is (= 2 (stream-car (stream-cdr s))))
+    (is (= 1 (stream-car (stream-cdr (stream-cdr s)))))
+    (is (= 3 (stream-car (stream-cdr (stream-cdr (stream-cdr s))))))
+    (is (= 2 (stream-car (stream-cdr (stream-cdr (stream-cdr (stream-cdr s)))))))
+    (is (= 1 (stream-car (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr s))))))))
+    (is (= 3 (stream-car (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr (stream-cdr s)))))))))
+    )
+  )
 
 (run-tests)
