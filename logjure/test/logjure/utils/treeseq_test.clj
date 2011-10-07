d53393182ddb46801f7efb2ab376d37c7e5a9fd9
Piotr Zielinski <zielinski_piotr@hotmail.com>
2011-10-07 10:53:09 -0400

    test-tree-seq-multi-depth more tests;

--- logjure/test/logjure/utils/treeseq_test.clj
+++ logjure/test/logjure/utils/treeseq_test.clj
@@ -845,6 +845,13 @@
             [:c :C]
             ) 
          (doall (tree-seq-multi-depth '(:a (:b) :c) '(:A (:B) :C)))))
+  (is (= '([(:a ((:b)) :c) (:A (:B) :C)] 
+            [:a :A]
+            [((:b)) (:B)]
+            [(:b) :B]
+            [:c :C]
+            ) 
+         (doall (tree-seq-multi-depth '(:a ((:b)) :c) '(:A (:B) :C)))))
   (is (= ['?x :x] (nth (tree-seq-multi-depth (deeply-nested 10000 '?x) (deeply-nested 10000 :x)) 10000)))
   )
 
