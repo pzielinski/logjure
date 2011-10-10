913fbffa2a27e44ed9983477ddd1510056b1cc11
Piotr Zielinski <zielinski_piotr@hotmail.com>
2011-10-10 03:06:07 -0400

    treeimpl/TreeNodeImpl implementation changed to support tree-map;

--- logjure/src/logjure/utils/treeimpl.clj
+++ logjure/src/logjure/utils/treeimpl.clj
@@ -4,34 +4,36 @@
     )
   )
 
-(defrecord TreeNodeImpl [value get-children-values])
+(defrecord TreeNodeImpl [value get-children-value-and-fn-pairs])
 
 (extend-type TreeNodeImpl
   TreeNode
   (node-is-leaf 
     [node] 
-    (empty? ((:get-children-values node) node)))
+    (empty? ((:get-children-value-and-fn-pairs node) node)))
   (node-get-children 
     [node] 
     (map 
-      (fn [child-value] (TreeNodeImpl. child-value (:get-children-values node))) 
-      ((:get-children-values node) node)))
+      (fn [[child-value child-get-children-value-and-fn-pairs]] 
+        (TreeNodeImpl. child-value child-get-children-value-and-fn-pairs)) 
+      ((:get-children-value-and-fn-pairs node) node)))
   (node-get-value 
     [node] 
     (:value node))
   )
 
 (defn create-tree
-  ""
+  "Creates new tree.
+get-children-values [parent-value] drives creation of the children nodes."
   [root-value get-children-values]
   (letfn 
-    [(get-children-values-x 
+    [(get-children-value-and-fn-pairs-x 
        [node] 
-       (get-children-values (node-get-value node)))]  
-    (TreeNodeImpl. root-value get-children-values-x))
+       (map (fn [value] [value get-children-value-and-fn-pairs-x]) (get-children-values (node-get-value node))))]  
+    (TreeNodeImpl. root-value get-children-value-and-fn-pairs-x))
   )
 
-(defn create-tree-infinite
+(defn create-infinite-tree
   "Creates infinite tree with each node having infinite children."
   []
   (create-tree
@@ -50,4 +52,18 @@
     (fn [s] 
       (get-children s)))
   ) 
+
+(defn tree-map
+  "Creates new tree with identical structure with each node mapped to a new node.
+mapping-fn [node-value]"
+  [mapping-fn root]
+  (letfn 
+    [(get-children-value-and-fn-pairs-x 
+       [original-parent-node new-parent-node] 
+       (map 
+         (fn [original-node] 
+           [(mapping-fn (node-get-value original-node)) (fn [n] (get-children-value-and-fn-pairs-x original-node n))]) 
+         (node-get-children original-parent-node)))]  
+    (TreeNodeImpl. (mapping-fn (node-get-value root)) (fn [n] (get-children-value-and-fn-pairs-x root n))))
+  )
 
