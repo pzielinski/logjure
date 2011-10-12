9c94759e75aca202f6b285628dd7120031b5f11f
Piotr Zielinski <zielinski_piotr@hotmail.com>
2011-10-11 21:58:02 -0400

    sequtil/deep-equal? for deep comparison of s forms expressed as clojure sequences, clojure = fails with stack overflow error when comparing two deeply nested sequences;

--- logjure/src/logjure/utils/sequtil.clj
+++ logjure/src/logjure/utils/sequtil.clj
@@ -1,11 +1,26 @@
-(ns logjure.utils.sequtil)
+(ns logjure.utils.sequtil
+  (:use 
+    logjure.utils.lazytree
+    logjure.utils.treeseq
+    logjure.utils.testing 
+    clojure.contrib.test-is
+    )
+)
 
-(map-reduce
+(defn map-reduce
   [f coll result]
   (lazy-seq
     (when-let 
-      [s (seq coll)
-       res (f (first s) result)]
-      (cons res (map-reduce f (rest s) res))))
+      [s (seq coll)]
+      (let 
+        [res (f (first s) result)]
+        (cons res (map-reduce f (rest s) res)))))
   )
 
+(defn deep-equal?
+  "Compares s forms."
+  [expr1 expr2]
+  (every? 
+    (fn [[v1 v2]] (= v1 v2)) 
+    (tree-seq-depth (tree-disjoin (seq-tree expr1) (seq-tree expr2) :not-found)))
+  )
